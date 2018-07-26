f55a095bafba75dbaf90bab77c07e0bc8b7a897d
Ross Rodwell <ross.rodwell@alces-software.com>
2018-07-26 12:44:03 +0100

    Renamed client installer from cl_add-host-to-cluster.sh to cl_installnrds.sh and fixed error

--- cl_installnrds.sh
+++ cl_installnrds.sh
@@ -36,7 +36,7 @@ if [ ! -f ${nrds_installer_dir}/installnrds.sh ]; then
     exit 1
 fi
 
-source ${nrds_installer_dir}/installnrds.sh ${nagiocheckhost} ${interval}
+${nrds_installer_dir}/installnrds.sh ${nagiocheckhost} ${interval}
 rc=$?
 if [ "${rc}" -ne "0" ]; then
     echo "Error! Something went wrong with the installation!"
